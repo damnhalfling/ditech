43c11563d855e6e14424e3e41e95abc6a7776781
davi.menegotto <menegotto.davi@gmail.com>
2016-06-27 08:34:08 -0300

    modificando estrutura de data e horas nas reservas das salas. separado o campo de hora do data, as horas agora estao como enum indo de hora em hora das 8 da manha as 18, desta maneira tenho controle sobre as horas desponiveis que antes não tinha

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -18,15 +18,17 @@ class ReservaController extends Controller
     public function criar() {
 
         $inputs = Input::all();
-        $date   = $inputs['data']."  ".$inputs['horaInicio'];
+        $data   = $inputs['data'];
+        $hora   = $inputs['horaInicio'];
 
-        $exists = Reserva::where('id_sala',$inputs['sala'])->where('datahora',$date)->first();
+        $exists = Reserva::where('id_sala',$inputs['sala'])->where('data',$data)->where('hora',$hora)->first();
 
         if (!$exists) {
             $reserva = new Reserva();
             $reserva->id_usuario = Auth::user()->id;
-            $reserva->id_sala = $inputs['sala'];
-            $reserva->datahora = $date;
+            $reserva->id_sala    = $inputs['sala'];
+            $reserva->data       = $data;
+            $reserva->hora       = $hora;
             $reserva->save();
         }
 
